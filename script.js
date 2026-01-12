1d245f43994e04fa5fb9e6e67759c3fb67bd6109
bianalfarysi <jobshiffaqalbianalfarysi@gmail.com>
2026-01-12 22:22:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn   = document.getElementById('showBtn');
+const wrapper = document.getElementById('menuWrapper');
+const music = document.getElementById('bgMusic');
+const toggle = document.getElementById('musicToggle');
+
+// tampilkan menu
+btn.addEventListener('click', () => {
+  wrapper.classList.remove('hidden');
+  btn.style.display = 'none';
+});
+
+// kontrol musik
+let isPlaying = true;
+toggle.addEventListener('click', () => {
+  if (isPlaying) {
+    music.pause();
+    toggle.textContent = '🔇';
+  } else {
+    music.play();
+    toggle.textContent = '🔊';
+  }
+  isPlaying = !isPlaying;
+});
